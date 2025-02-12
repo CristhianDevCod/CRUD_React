980cc007bc542eca4a834933c2f48647fd10871f
CristhianDavid <cristhian19990302@gmail.com>
2025-02-12 14:34:23 -0500

    CR

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,162 @@
 import FormControl from '@mui/material/FormControl';
-import { TextField } from '@mui/material';
+import { TextField, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import Grid2  from '@mui/material/Grid2/Grid2.js';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import Axios from 'axios'
+
+import Table from '@mui/material/Table';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell, {tableCellClasses} from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
+import Paper from '@mui/material/Paper';
+
+// -----------------
+import '@fontsource/roboto/500.css';
 
 function App() {
+  const [nombre, setNombre] = useState('');
+  const [edad, setEdad] = useState(0);
+  const [pais, setPais] = useState('');
+  const [cargo, setCargo] = useState('');
+  const [experiencia, setExperiencia] = useState(0);
+
+  const [empleadosLista, setEmpleados] = useState([])
+
+  const add = (event) => {
+    event.preventDefault()
+    Axios.post("http://localhost:3001/create",{
+      nombre:nombre, edad:edad, pais:pais, cargo:cargo, experiencia:experiencia
+    }).then(()=>{
+      alert('Empleado creado')
+    });
+  }
+
+  const get = () => {
+    Axios.get("http://localhost:3001/empleados",).then((response)=>{
+        setEmpleados(response.data)
+    });
+  }
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
 
   return (
     <>
       <Container fixed>
-        <h1>Pruebas</h1>
+      <Typography variant="h3" component="h3">
+        Pruebas
+      </Typography>
+
         <FormControl>
           <Grid2 container spacing={4}>
             <Grid2 size={6}>
-              <TextField id="nombre" label="Nombre" variant="outlined" />
+              <TextField 
+                id="nombre"  
+                label="Nombre" 
+                variant="outlined" 
+                onChange={(event) => (setNombre(event.target.value))}
+              />
             </Grid2>
+
             <Grid2 size={6}>
-              <TextField id="edad" label="Edad" variant="outlined" />
+              <TextField 
+                id="edad"  
+                label="Edad" 
+                variant="outlined" 
+                type='number' 
+                onChange={(event) => (setEdad(event.target.value))}
+              />
+            </Grid2>
+
+            <Grid2 size={6}>
+            <TextField 
+              id="pais"  
+              label="Pais" 
+              variant="outlined" 
+              onChange={(event) => (setPais(event.target.value))}
+            />
+            </Grid2>
+
+            <Grid2 size={6}>
+              <TextField 
+                id="cargo"  
+                label="Cargo" 
+                variant="outlined" 
+                onChange={(event) => (setCargo(event.target.value))}
+              />
+            </Grid2>
+
+            <Grid2 size={12} container="true" direction="row" sx={{ justifyContent: "center", alignItems: "center"}}>
+              <TextField 
+                id="experiencia"  
+                label="Experiencia" 
+                variant="outlined" 
+                type='number'
+                onChange={(event) => (setExperiencia(event.target.value))}
+              />
+            </Grid2>
+
+            <Grid2 size={12} container="true" direction={"row"} sx={{justifyContent: "center", alignItems:"center"}}>
+              <Button 
+                variant="contained"
+                color='warning'
+                onClick={add}
+                type='submit'
+              >Agregar persona</Button>
+              <Button 
+                variant="contained"
+                color='info'
+                onClick={get}
+                type='submit'
+              >Listar personas</Button>
             </Grid2>
           </Grid2>
         </FormControl>
+      </Container>
+
+      <Container fixed sx={{marginTop:'30px'}}>
+        <Grid2 size={12}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Nombre</StyledTableCell>
+                  <StyledTableCell>Edad</StyledTableCell>
+                  <StyledTableCell>Pais</StyledTableCell>
+                  <StyledTableCell>Cargo</StyledTableCell>
+                  <StyledTableCell>Experiencia</StyledTableCell>
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {empleadosLista.map((persona) => (
+                  <TableRow
+                    key={persona.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell component="th" scope="row">
+                      {persona.nombre}
+                    </TableCell>
+                    <TableCell>{persona.edad}</TableCell>
+                    <TableCell>{persona.pais}</TableCell>
+                    <TableCell>{persona.cargo}</TableCell>
+                    <TableCell>{persona.experiencia}</TableCell>
+                  </TableRow>))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid2>
       </Container>
     </>
   )
