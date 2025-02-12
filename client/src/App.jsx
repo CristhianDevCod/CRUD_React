c0a99150cd8eac0e5c35acc2d32bab2232b8c347
Cristhian <cristhian19990302@gmail.com>
2025-02-11 22:40:09 -0500

    implementaciones de material ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
 import FormControl from '@mui/material/FormControl';
-import { InputLabel, TextField } from '@mui/material';
+import { TextField } from '@mui/material';
+import Grid2  from '@mui/material/Grid2/Grid2.js';
+import Container from '@mui/material/Container';
 
 function App() {
 
   return (
     <>
-      <h1>Pruebas</h1>
-      <FormControl>
-        <TextField id="nombre" label="Nombre" variant="outlined" />
-        <TextField id="nombre" label="Nombre" variant="outlined" />
-      </FormControl>
+      <Container fixed>
+        <h1>Pruebas</h1>
+        <FormControl>
+          <Grid2 container spacing={4}>
+            <Grid2 size={6}>
+              <TextField id="nombre" label="Nombre" variant="outlined" />
+            </Grid2>
+            <Grid2 size={6}>
+              <TextField id="edad" label="Edad" variant="outlined" />
+            </Grid2>
+          </Grid2>
+        </FormControl>
+      </Container>
     </>
   )
 }
