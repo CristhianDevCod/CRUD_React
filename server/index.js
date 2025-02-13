f5a87ddf6291b0fbcca8d8e94b6247ee0a35cfdc
CristhianDavid <cristhian19990302@gmail.com>
2025-02-13 12:17:18 -0500

    proyecto finalizado

--- server/index.js
+++ server/index.js
@@ -14,6 +14,19 @@ const db = mysql.createConnection({
     database: "empleados_crud"
 });//Creacion de una conecion 
 
+//Funcion para envolver las consultas en promesas
+const queryAsync = (query, values) => {
+    return new Promise((resolve, reject) => {
+        db.query(query, values, (err, result) => {
+            if(err){
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        })
+    })
+}
+
 //Create
 app.post("/create", (req, res) => {
     const nombre = req.body.nombre;
@@ -32,14 +45,26 @@ app.post("/create", (req, res) => {
 });
 
 //Read
-app.get("/empleados", (req, res) => {
-    db.query('SELECT * FROM empleados', (err, result)=>{
-        if(err){
-            console.log(err)
-        } else {
-            res.send(result)
-        }
-    })
+//Se implementa mejoras con async/await y manejo adecuado de errors
+app.get("/empleados", async (req, res) => {
+    try {
+        //Se ejecuta la consulta a la base de datos
+        const result = await queryAsync('SELECT * FROM empleados');
+
+        //Se reponde con los resultados y un código de estado 200
+        res.status(200).json(result)
+    } catch (err){
+        //Si ocurre un error, se responde con un código de estado 500 y un mensaje de error
+        console.error('Error al obtener empleados: ', err);
+        res.status(500).json({message: 'Error en el servidor, intenta más tarde.'});
+    }
+    // db.query('SELECT * FROM empleados', (err, result)=>{
+    //     if(err){
+    //         console.log(err)
+    //     } else {
+    //         res.send(result)
+    //     }
+    // })
 });
 
 //Update
@@ -52,12 +77,26 @@ app.put("/update", (req, res) => {
     const experiencia = req.body.experiencia;
 
     db.query('UPDATE empleados SET nombre=?, edad=?, pais=?, cargo=?, experiencia=? WHERE id=?;', 
-        [nombre, edad, pais, cargo, experiencia], 
+        [nombre, edad, pais, cargo, experiencia, id], 
         (err, result)=>{
         if(err){
             console.log(err)
         } else {
-            res.send("Empleado actualizado con exito")
+            res.send(result)
+        }
+    })
+});
+
+//Delete
+app.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+
+    db.query('DELETE FROM empleados WHERE id=?;',id, 
+        (err, result)=>{
+        if(err){
+            console.log(err)
+        } else {
+            res.send(result)
         }
     })
 });
